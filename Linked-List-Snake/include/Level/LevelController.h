a5840b240b0fd1ece59d5fe9a8dec2b7b51b2fff
Dip Patel <pateldeep2332@gmail.com>
2025-02-10 14:03:42 +0530

    Adding Level Model and Controller

--- Linked-List-Snake/include/Level/LevelController.h
+++ Linked-List-Snake/include/Level/LevelController.h
@@ -1,8 +1,17 @@
 #pragma once
+#include "../../include/Level/LevelModel.h"
+#include "../../include/Level/LevelView.h"
 
 namespace Level {
 
 	class LevelController {
+
+	private:
+		LevelModel* level_model;
+		LevelView* level_view;
+
+		void destroy();
+
 	public:
 		LevelController();
 		~LevelController();
@@ -10,6 +19,9 @@ namespace Level {
 		void initialize();
 		void update();
 		void render();
+
+		float getCellWidth();
+		float getCellHeight();
 	};
 
 }
--- Linked-List-Snake/source/Level/LevelController.cpp
+++ Linked-List-Snake/source/Level/LevelController.cpp
@@ -3,22 +3,39 @@
 namespace Level {
 
 	LevelController::LevelController() {
-
+		level_model = new LevelModel();
+		level_view = new LevelView();
 	}
 
 	LevelController::~LevelController() {
-
+		destroy();
 	}
 
 	void LevelController::initialize() {
-
+		level_view->initialize();
+		level_model->initialize(level_view->getGridWidth(), level_view->getGridHeight());
 	}
 
 	void LevelController::update() {
-
+		level_view->update();
 	}
 
 	void LevelController::render() {
+		level_view->render();
+	}
 
+	void LevelController::destroy() {
+		delete level_model;
+		delete level_view;
+	}
+
+	float LevelController::getCellWidth()
+	{
+		return level_model->getCellWidth();
+	}
+
+	float LevelController::getCellHeight()
+	{
+		return level_model->getCellHeight();
 	}
 }
