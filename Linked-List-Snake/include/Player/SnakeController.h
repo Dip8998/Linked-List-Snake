dce494ca75c45a6e9bda3c707844730a7b322d04
Dip Patel <pateldeep2332@gmail.com>
2025-02-10 18:11:49 +0530

    Setup snake

--- Linked-List-Snake/include/Player/SnakeController.h
+++ Linked-List-Snake/include/Player/SnakeController.h
@@ -2,7 +2,25 @@
 
 namespace Player {
 
+	enum class SnakeState {
+		ALIVE,
+		DEAD,
+	};
+
 	class SnakeController {
+	private:
+		const int initial_snake_length = 10;
+		SnakeState current_snake_state;
+
+		void processPlayerInput();
+		void updateSnakeDirection();
+		void moveSnake();
+		void processSnakeCollision();
+		void handleRestart();
+		void reset();
+		void destroy();
+
+
 	public:
 		SnakeController();
 		~SnakeController();
@@ -10,6 +28,11 @@ namespace Player {
 		void initialize();
 		void update();
 		void render();
+
+		void spawnSnake();
+		void respawnSnake();
+		void setSnakeState(SnakeState state);
+		SnakeState getSnakeState();
 	};
 
 }
--- Linked-List-Snake/source/Player/PlayerService.cpp
+++ Linked-List-Snake/source/Player/PlayerService.cpp
@@ -28,7 +28,7 @@ namespace Player {
 	}
 
 	void PlayerService::spawnPlayer() {
-
+		snake_controller->spawnSnake();
 	}
 
 	void PlayerService::destroy() {
