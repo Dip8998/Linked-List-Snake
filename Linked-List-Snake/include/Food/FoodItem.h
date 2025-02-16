a5371ea84f59ecef06f549435480bf0af48b04e5
Dip Patel <pateldeep2332@gmail.com>
2025-02-16 11:31:49 +0530

    Starting off with Food

---
+++ Linked-List-Snake/include/Food/FoodItem.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "UI/UIElement/ImageView.h"
+#include "FoodType.h"
+
+namespace Food {
+
+	class FoodItem {
+	private:
+		UI::UIElement::ImageView* food_image;
+		sf::Vector2i grid_position;
+		float cell_width;
+		float cell_height;
+		FoodType food_type;
+
+		void initializeFoodImage();
+
+		sf::String getFoodTexturePath();
+		sf::Vector2f getFoodImagePosition();
+
+	public:
+		FoodItem();
+		~FoodItem();
+
+		static const int number_of_foods = 8;
+
+		void initialize(sf::Vector2i grid_pos, float width, float height, FoodType type);
+		void  update();
+		void render();
+
+		FoodType getFoodType();
+
+	};
+
+}
