ea3765bbe47f3ad3b3bca2b57718408bd56e3c2e
Dip Patel <pateldeep2332@gmail.com>
2025-02-23 15:23:43 +0530

    Deleted at Beginning - DLL

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -134,6 +134,19 @@ namespace LinkedListLib {
 
 		}
 
+		void DoubleLinkedList::removeNodeAtHead() {
+			linked_list_size--;
+
+			Node* cur_node = head_node;
+			head_node = head_node->next;
+
+			if (head_node != nullptr) {
+				static_cast<DoubleNode*>(head_node)->previous = nullptr;
+			}
+
+			cur_node->next = nullptr;
+			delete cur_node;
+		}
 	}
 
 }
