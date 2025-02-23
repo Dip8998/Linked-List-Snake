75ccf98c67b9edf63eefb6fefa610bcb7cf041b6
Dip Patel <pateldeep2332@gmail.com>
2025-02-23 15:13:07 +0530

    Inserted at Beginning - DLL

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -1,4 +1,5 @@
 #include "LinkedListLib/DoubleLinked/DoubleLinkedList.h"
+#include "LinkedListLib/DoubleLinked/DoubleNode.h"
 
 namespace LinkedListLib {
 
@@ -21,6 +22,32 @@ namespace LinkedListLib {
 		void DoubleLinked::shiftNodesAfterRemoval(Node* cur_node)
 		{
 
+		}
+
+		Node* DoubleLinkedList::createNode()
+		{
+			return new DoubleNode();
+		}
+
+		void DoubleLinkedList::insertNodeAtHead()
+		{
+			linked_list_size++;
+			Node* new_node = createNode();
+
+			if (head_node == nullptr)
+			{
+				head_node = new_node;
+				static_cast<DoubleNode*>(new_node)->previous = nullptr;
+				initializeNode(new_node, nullptr, Operation::HEAD);
+				return;
+			}
+
+			initializeNode(new_node, head_node, Operation::HEAD);
+
+			new_node->next = head_node;
+			static_cast<DoubleNode*>(head_node)->previous = new_node;
+
+			head_node = new_node;
 		}
 	}
 
