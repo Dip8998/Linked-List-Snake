0c7e69f68df7ac35fa165662730a42263f12cb10
Dip Patel <pateldeep2332@gmail.com>
2025-02-23 11:51:22 +0530

    Refactored Single Link List

--- Linked-List-Snake/include/LinkedListLib/SingleLinked/SingleLinkedList.h
+++ Linked-List-Snake/include/LinkedListLib/SingleLinked/SingleLinkedList.h
@@ -1,70 +1,41 @@
 #pragma once
 #include <SFML/System/Vector2.hpp>
-#include "LinkedList/Node.h"
-#include "../Player/Direction.h"
+#include "LinkedListLib/Node.h"
+#include "LinkedListLib/LinkedList.h"
 
-namespace LinkedList {
-
-	enum class Operation
+namespace LinkedListLib
+{
+	namespace SingleLinked
 	{
-		HEAD,
-		MID,
-		TAIL,
-	};
+		class SingleLinkedList : public LinkedList
+		{
+		protected:
+			virtual Node* createNode() override;
 
-	class SingleLinkedList {
-
-	private:
-		Node* head_node;
-
-		float node_width;
-		float node_height;
-
-		sf::Vector2i default_position;
-		Direction default_direction;
-
-		Node* createNode();
-
-		int linked_list_size;
-
-	public:
-		SingleLinkedList();
-		~SingleLinkedList();
-
-		void initialize(float width, float height, sf::Vector2i position, Direction direction);
-		void render();
-
-		void insertNodeAtTail();
-		void insertNodeAtIndex(int index);
-		void shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node);
-		void insertNodeAtMiddle();
-		int findMiddleNode();
-		void updateNodeDirection(Direction direction_to_set);
-		void updateNodePosition();
-		bool processNodeCollision();
-		void removeNodeAtHead();
-		void removeAllNodes();
-		Node* getHeadNode();
-		void shiftNodesAfterRemoval(Node* cur_node);
-		void removeNodeAt(int index);
-		void removeNodeAtIndex(int index);
-		void removeNodeAtMiddle();
-
-		void removeNodeAtTail();
-		Node* findNodeAtIndex(int index);
-		void removeHalfNodes();
-
-		Direction getReverseDirection(Direction reference_direction);
-		void reverseNodeDirections();
-		Direction reverse();
-
-		std::vector<sf::Vector2i> getNodesPositionList();
-		sf::Vector2i getNewNodePosition(Node* reference_node, Operation operation);
-		void initializeNode(Node* new_node, Node* reference_node, Operation operation);
-
-		void insertNodeAtHead();
-
-	};
+		public:
+			SingleLinkedList();
+			~SingleLinkedList();
 
 
+			void insertNodeAtTail() override;
+			void insertNodeAtHead() override;
+			void insertNodeAtMiddle() override;
+			void insertNodeAtIndex(int index);
+
+			void shiftNodesAfterInsertion(Node* new_node, Node* cur_node, Node* prev_node);
+
+			void removeNodeAtTail() override;
+			void removeNodeAtHead() override;
+			void removeNodeAtMiddle() override;
+			void removeNodeAt(int index) override;
+			void removeNodeAtIndex(int index);
+			void removeAllNodes() override;
+			void removeHalfNodes() override;
+
+			void shiftNodesAfterRemoval(Node* cur_node);
+
+			Direction reverse() override;
+			void reverseNodeDirections();
+		};
+	}
 }
